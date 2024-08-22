ec2e687a43114e81aa6fabe947d187edae423eb6
Eamann Miled <130555562+Eamanmiled@users.noreply.github.com>
2024-08-22 02:07:12 +0100

    quick lil q before i go bed

---
+++ Two_Pointers/Valid_Pal.py
@@ -0,0 +1,30 @@
+s = "Was it a car or a cat I saw?"
+s = s.replace(" ", "")
+s = s.strip("?")
+s = s.lower()
+
+
+def compare(l, r):
+    i = 0
+    while i < len(l):
+        if l[i] == r[-i -1]:
+            i += 1
+        else:
+            return False
+    return True
+
+
+if len(s) % 2 == 0:
+    midpoint = len(s) // 2
+    print(s[:midpoint], s[midpoint:])
+    if compare(s[:midpoint], s[midpoint:]):
+        print("its a palidrome")
+    else:
+        print("nope")
+else:
+    midpoint = len(s) // 2
+    print(s[:midpoint], s[midpoint + 1:])
+    if compare(s[:midpoint], s[midpoint:]):
+        print("its a palidrome")
+    else:
+        print("nope")
